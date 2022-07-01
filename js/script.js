f8bc8cd8bf318b44f8ddd8c371a54e3ebe96c4ae
Henrique <henrique.assispera@gmail.com>
2022-07-01 10:11:53 -0300

    Reestruturando JS

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 /*
-fetch('https://618e6a1850e24d0017ce1294.mockapi.io/api/v1/products').then(resposta=>{
+function adicionarCarrinho (objeto, quantidade, carrinho) {
 
-   return resposta.json();
+        let carrinho.find()
 
-}).then(corpo=>{
+       return carrinho.push({
+            produto:objeto, 
+            quantidade:quantidade,           
+             
+        });
 
-    console.log(corpo);    
-})*/
+}*/
 
-/*fazendo conexão com a url*/
 function fazGet(url) {
     let request = new XMLHttpRequest()
     request.open("GET", url, false)
@@ -16,47 +18,64 @@ function fazGet(url) {
     return request.responseText
 }
 
-/* montando função de população*/ 
-function criaDivisao(usuario) {
-    console.log(usuario)
-    
-    divisao = document.createElement("div");
-    
-    tagImage = document.createElement('img');
-    tagNome = document.createElement("h2");
-    tagPrice = document.createElement("h2");
-    
-   
-    tagImage.innerHTML = usuario.image
-    tagNome.innerHTML = usuario.name
-    tagPrice.innerHTML = usuario.price
-   
 
 
-    divisao.appendChild(tagImage);
-    divisao.appendChild(tagNome);
-    divisao.appendChild(tagPrice);
-   
 
+//Carrinho atualizando
+atualizarCarrinho = (val) =>{
 
-    return divisao;
-}
-
-
-/*montando função main */
+            console.log(val);
+            var containerCarrinho = document.getElementById('Carrinho');
+            containerCarrinho.innerHTML = "";
+            val.forEach((val) => {
+                    
+			if(val.quantidade >0){
+			containerCarrinho.innerHTML+= `
+			<p>`+val.name+` |quantidade: `+val.quantidade+`</p>
+			
+		`;
+                
+            }
+		})
+    }
 
 
 function main() {
-    let data = fazGet("https://618e6a1850e24d0017ce1294.mockapi.io/api/v1/products");
-    let usuarios = JSON.parse(data);
-    let produto = document.getElementById("produto");
-    usuarios.forEach(element => {
-        let divisao = criaDivisao(element);
-        divisoria.appendChild(divisao);
+    let data = fazGet("https://618e6a1850e24d0017ce1294.mockapi.io/api/v1/products/");
+    const val = JSON.parse(data);
+    var containerProdutos = document.getElementById("Produtos");   
+
+    val.forEach(val => {
+        containerProdutos.innerHTML+=`
+        <div class="produto-single">
+            <img src="https://loremflickr.com/640/480/food" />
+            <a key = "`+val.id+`" href ="#"> + </a>
+            <p>`+val.name+`</p>
+            <p>`+val.price+`</p>
+            
+        </div>`
     });
-        // Para cada usuario
-        // criar uma divisao
-        // adicionar no produto
+
+    
+
+    //registrando item no carrinho
+    var quantidade=0;
+    var links = document.getElementsByTagName('a');
+
+    
+    for (var i= 0; i < links.length; i++) {
+        links[i].addEventListener("click",function(){
+            
+            let key = this.getAttribute('key');
+            val[key].quantidade++;
+            atualizarCarrinho(val);
+            return false;
+            
+	})
+	
+	
+}
+    
 }
 
 main()
